e1019bbf66a56d9e9977261eca517c8432205ff8
Jazzcactus <justin.jennissen@gmail.com>
2019-10-01 15:16:18 -0500

    Baby homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      Pretty Cool Podcast Homepage
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Episodes:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
