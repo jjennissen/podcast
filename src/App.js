5f6c1b5419eba2689de7ed7b60353f27226394b6
Jazzcactus <justin.jennissen@gmail.com>
2019-10-02 12:38:23 -0500

    worlds greatest page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      Pretty Cool Podcast Homepage
+      Pretty Cool Haircut Podcast Homepage
         <p>
         Episodes:
         </p>
+          <div class="box red">
+          Episode 1
+          </div>
+          <p></p>
+          <div className="box red">
+              Episode 2
+          </div>
+          <p></p>
+
+          <div className="box red">
+              Episode 3
+          </div>
       </header>
+        <body>
+        </body>
     </div>
   );
 }
