6c475f37b78f302181544301d96bc231074df1d0
Jazzcactus <justin.jennissen@gmail.com>
2019-10-02 13:58:09 -0500

    Added Material-UI and some boxes and minor tweaks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import './App.css';
+import Box from '@material-ui/core/Box';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      Pretty Cool Haircut Podcast Homepage
-        <p>
-        Episodes:
-        </p>
-          <div class="box red">
-          Episode 1
-          </div>
-          <p></p>
-          <div className="box red">
+          <h1>Hamborne Haircut Podcast Homepage</h1>
+            <h2>Episodes:</h2>
+          <Box component="span" border={1} bgcolor="primary.main" m={1} p={1} paddingLeft={5}
+          paddingRight={5}>
+              Episode 0
+          </Box>
+          <Box component="span" border={1} bgcolor="primary.main" m={1} p={1} paddingLeft={5}
+               paddingRight={5}>
+              Episode 1
+          </Box>
+          <Box component="span" border={1} bgcolor="primary.main" m={1} p={1} paddingLeft={5}
+               paddingRight={5}>
               Episode 2
-          </div>
-          <p></p>
-
-          <div className="box red">
+          </Box>
+          <Box component="span" border={1} bgcolor="primary.main" m={1} p={1} paddingLeft={5}
+               paddingRight={5}>
               Episode 3
-          </div>
+          </Box>
       </header>
         <body>
         </body>
